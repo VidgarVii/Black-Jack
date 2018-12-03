5da7d2fb40e1aeca3e05667f674c35097fd21ad1
Pavel <vidasp@mail.ru>
2018-12-03 23:27:10 +0300

    refact deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,13 +6,10 @@ class Deck
   end
 
   def create_deck
-    %w[2 3 4 5 6 7 8 9 10 J Q K A].each do |value|
-      @cards << Card.new(value, '♣')
-      @cards << Card.new(value, '♠')
-      @cards << Card.new(value, '♥')
-      @cards << Card.new(value, '♦')
+    %w[2 3 4 5 6 7 8 9 10 J Q K A].map do |value|
+      %w[♣ ♠ ♦ ♥].map { |suit| @cards << Card.new(value, suit) }
     end
-  end  
+  end
 
   def take_card
     @cards.pop
--- lib/hand.rb
+++ lib/hand.rb
@@ -2,4 +2,7 @@ class Hand
   def initialize(player, card)
     
   end
+
+  def score
+  end
 end
