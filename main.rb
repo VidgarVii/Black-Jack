afd6871ce15ef0f0488f57128bbe2acae8204be8
VidgarVii <vidasp@mail.ru>
2018-12-03 15:14:30 +0300

    clear

--- main.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'lib/modules/validations'
-require_relative 'lib/card'
-require_relative 'lib/gamer'
-require_relative 'lib/dealer'
-require_relative 'lib/black_jack'
-require_relative 'lib/interface'
-
-@start = Interface.new
