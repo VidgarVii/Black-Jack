a6adc542706e1695d59e3fed8d430e666939f51c
Pavel <vidasp@mail.ru>
2018-12-03 23:13:55 +0300

    refact deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,14 +1,18 @@
 class Deck
   def initialize
     @cards = []
+    create_deck
+    @cards.shuffle!
+  end
+
+  def create_deck
     %w[2 3 4 5 6 7 8 9 10 J Q K A].each do |value|
       @cards << Card.new(value, '♣')
       @cards << Card.new(value, '♠')
       @cards << Card.new(value, '♥')
       @cards << Card.new(value, '♦')
     end
-    @cards.shuffle!
-  end
+  end  
 
   def take_card
     @cards.pop
