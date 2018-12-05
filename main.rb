117991d26f9f9436ad460405b085d04dba313f51
VidgarVii <vidasp@mail.ru>
2018-12-05 22:47:24 +0300

    order

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
-require_relative 'lib/card'
-require_relative 'lib/deck'
-require_relative 'lib/hand'
-require_relative 'lib/player'
-require_relative 'lib/dealer'
-require_relative 'lib/bank'
-require_relative 'lib/black_jack'
+require_relative 'lib/models/card'
+require_relative 'lib/models/deck'
+require_relative 'lib/models/hand'
+require_relative 'lib/models/player'
+require_relative 'lib/models/dealer'
+require_relative 'lib/models/bank'
+require_relative 'lib/controllers/black_jack'
 require_relative 'lib/ui/interface'
 
 game = BlackJack.new
